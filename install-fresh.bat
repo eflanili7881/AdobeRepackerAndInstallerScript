fcd9cfa738227a1629789bfd2a0276050512d957
osmankovan123 <44976117+osmankovan123@users.noreply.github.com>
2023-05-04 09:09:37 +0300

    Add files via upload

---
+++ install-fresh.bat
@@ -0,0 +1,81 @@
+@echo off
+rem #####################
+rem # Preparation Phase #
+rem #####################
+cd %~dp0
+echo "==> Setting Variables ..."
+set adobetempinstaller=%systemdrive%\adobetempinstaller
+set excludefolder=%systemdrive%\excludefolder
+echo "==> Deleting Previously Failed Instance ..."
+rd %excludefolder% /s /q
+rd %adobetempinstaller% /s /q
+echo "==> Creating Directories ..."
+mkdir %excludefolder%
+mkdir %adobetempinstaller%
+mkdir %adobetempinstaller%\packages
+mkdir %adobetempinstaller%\packages\CCM
+mkdir %adobetempinstaller%\packages\core
+mkdir %adobetempinstaller%\packages\D6
+mkdir %adobetempinstaller%\packages\DECore
+mkdir %adobetempinstaller%\packages\DWA
+mkdir %adobetempinstaller%\packages\IPC
+mkdir %adobetempinstaller%\packages\LWA
+mkdir %adobetempinstaller%\packages\P6
+mkdir %adobetempinstaller%\packages\P7
+mkdir %adobetempinstaller%\packages\UWA
+mkdir %adobetempinstaller%\payloads
+mkdir %adobetempinstaller%\payloads\AdobeSpeedGrade9AllTrial
+mkdir %adobetempinstaller%\payloads\AdobeSpeedGrade9de_DELanguagePack
+mkdir %adobetempinstaller%\payloads\AdobeSpeedGrade9en_USLanguagePack
+mkdir %adobetempinstaller%\payloads\AdobeSpeedGrade9es_ESLanguagePack
+mkdir %adobetempinstaller%\payloads\AdobeSpeedGrade9fr_FRLanguagePack
+mkdir %adobetempinstaller%\payloads\AdobeSpeedGrade9it_ITLanguagePack
+mkdir %adobetempinstaller%\payloads\AdobeSpeedGrade9ja_JPLanguagePack
+mkdir %adobetempinstaller%\payloads\AdobeSpeedGrade9pt_BRLanguagePack
+mkdir %adobetempinstaller%\payloads\AdobeSpeedGrade9ru_RULanguagePack
+echo "==> Copying Exclusion File ..."
+copy "%~dp0exclude.txt" "%excludefolder%\exclude.txt"
+rem #####################
+rem # Compression Phase #
+rem #####################
+echo "==> Compressing Unpacked Products ..."
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\packages\CCM\CCM.pima" -mx5 -r "%~dp0\packages\CCM\CCM\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\packages\core\PDApp.pima" -mx5 -r "%~dp0\packages\core\PDApp\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\packages\D6\D6.pima" -mx5 -r "%~dp0\packages\D6\D6\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\packages\DECore\DECore.pima" -mx5 -r "%~dp0\packages\DECore\DECore\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\packages\DWA\DWA.pima" -mx5 -r "%~dp0\packages\DWA\DWA\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\packages\IPC\IPC.pima" -mx5 -r "%~dp0\packages\IPC\IPC\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\packages\LWA\LWA.pima" -mx5 -r "%~dp0\packages\LWA\LWA\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\packages\P6\P6.pima" -mx5 -r "%~dp0\packages\P6\P6\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\packages\P7\P7.pima" -mx5 -r "%~dp0\packages\P7\P7\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\packages\UWA\UWA.pima" -mx5 -r "%~dp0\packages\UWA\UWA\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\payloads\AdobeSpeedGrade9AllTrial\Assets1_1.zip" -mx5 -r "%~dp0payloads\AdobeSpeedGrade9AllTrial\Assets1_1\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\payloads\AdobeSpeedGrade9AllTrial\Assets2_1.zip" -mx5 -r "%~dp0payloads\AdobeSpeedGrade9AllTrial\Assets2_1\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\payloads\AdobeSpeedGrade9de_DELanguagePack\Assets2_1.zip" -mx5 -r "%~dp0payloads\AdobeSpeedGrade9de_DELanguagePack\Assets2_1\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\payloads\AdobeSpeedGrade9en_USLanguagePack\Assets2_1.zip" -mx5 -r "%~dp0payloads\AdobeSpeedGrade9en_USLanguagePack\Assets2_1\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\payloads\AdobeSpeedGrade9es_ESLanguagePack\Assets2_1.zip" -mx5 -r "%~dp0payloads\AdobeSpeedGrade9es_ESLanguagePack\Assets2_1\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\payloads\AdobeSpeedGrade9fr_FRLanguagePack\Assets2_1.zip" -mx5 -r "%~dp0payloads\AdobeSpeedGrade9fr_FRLanguagePack\Assets2_1\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\payloads\AdobeSpeedGrade9it_ITLanguagePack\Assets2_1.zip" -mx5 -r "%~dp0payloads\AdobeSpeedGrade9it_ITLanguagePack\Assets2_1\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\payloads\AdobeSpeedGrade9ja_JPLanguagePack\Assets2_1.zip" -mx5 -r "%~dp0payloads\AdobeSpeedGrade9ja_JPLanguagePack\Assets2_1\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\payloads\AdobeSpeedGrade9pt_BRLanguagePack\Assets2_1.zip" -mx5 -r "%~dp0payloads\AdobeSpeedGrade9pt_BRLanguagePack\Assets2_1\*"
+"c:\program files\7-zip\7z" a -bd -tzip "%adobetempinstaller%\payloads\AdobeSpeedGrade9ru_RULanguagePack\Assets2_1.zip" -mx5 -r "%~dp0payloads\AdobeSpeedGrade9ru_RULanguagePack\Assets2_1\*"
+rem ###########################
+rem # Copying Installer Phase #
+rem ###########################
+echo "==> Copying Installer ..."
+xcopy /q /e /h "%~dp0*" "%adobetempinstaller%" /exclude:%excludefolder%\exclude.txt
+rem ######################
+rem # Installation Phase #
+rem ######################
+echo "==> Installing Product ..."
+call "%adobetempinstaller%\Set-up.exe"
+echo "==> Waiting For Installation To Be Completed ..."
+echo "After installation completed, press any key ..."
+pause
+rem ##################
+rem # Clean-up Phase #
+rem ##################
+echo "==> Cleaning Up ..."
+rd %adobetempinstaller% /s /q
+rd %excludefolder% /s /q
+pause
