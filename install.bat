ec9a95151092d134d732dbcfe1b0c35cc86fce99
Osman Kovan <44976117+osmankovan123@users.noreply.github.com>
2023-06-07 22:20:58 +0300

    Improved exclusion.txt and compression.txt creation.
    Do not require remove all spaces.

--- install.bat
+++ install.bat
@@ -17,20 +17,16 @@ cd "%~dp0products"
 for /d %%a in ("*") do mkdir "%adobetempinstaller%\products\%%a"
 cd "%~dp0"
 echo "==> Creating Exclusion File ..."
-echo "Be sure to maximize shell window here, because get-childitem will clip directory names if you don't maximize the shell window."
-pause
-powershell -command "get-childitem -path %~dp0products -recurse -directory -depth 1 | select FullName" > %adobeworkfolder%\exclude.txt
-echo "Remove first 3 line from get-childitem, remove main folders, remove all things before products that not including \ before on products folder and remove all spaces from file and save it."
+powershell -command "start-transcript -path "%adobeworkfolder%\exclude.txt"; get-childitem -path "%~dp0products" -recurse -directory -depth 1 | foreach-object { Write-Host $_.FullName }; stop-transcript"
+echo "Remove all lines but file/folder paths, remove main folders, remove all things before products that not including \ before on products folder and save it."
 notepad %adobeworkfolder%\exclude.txt
 pause
 rem #####################
 rem # Compression Phase #
 rem #####################
 echo "==> Compressing Unpacked Products ..."
-echo "Be sure to maximize shell window here, because get-childitem will clip directory names if you don't maximize the shell window."
-pause
-powershell -command "get-childitem -path .\products -recurse -directory -depth 1 | select FullName" > %adobeworkfolder%\compress.txt
-echo "Remove first 3 line from get-childitem, remove main folders, remove all things before products that including \ before on products folder and remove all spaces from file and save it."
+powershell -command "start-transcript -path %adobeworkfolder%\compress.txt; get-childitem -path .\products -recurse -directory -depth 1 | foreach-object { Write-Host $_.FullName }; stop-transcript"
+echo "Remove all lines but file/folder paths, remove main folders, remove all things before products that including \ before on products folder, copy first line to one below to avoid an anomaly and save it."
 notepad %adobeworkfolder%\compress.txt
 pause
 for /f "usebackq delims=" %%b in ("%adobeworkfolder%\compress.txt") do "C:\Program Files\7-Zip\7z.exe" a -bd -tzip "%adobetempinstaller%\%%b.zip" -mx5 -r "%~dp0%%b\*"
